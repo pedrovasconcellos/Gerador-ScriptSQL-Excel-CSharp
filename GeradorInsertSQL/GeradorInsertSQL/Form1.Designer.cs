84caea9670b8eeb1cc8a14a184f328f2f63a7f65
Pedro Vasconcellos <pedrovasconcellossp@gmail.com>
2017-07-11 04:30:12 +0000

    2017-07-11 ScriptSQL

---
+++ GeradorInsertSQL/GeradorInsertSQL/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace GeradorInsertSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnImportarPlanilha = new System.Windows.Forms.Button();
+            this.btnAbrirPlanilha = new System.Windows.Forms.Button();
+            this.cbxTipoDeExcel = new System.Windows.Forms.ComboBox();
+            this.txtNomeAquivo = new System.Windows.Forms.TextBox();
+            this.lblNomeArquivo = new System.Windows.Forms.Label();
+            this.lblDescricao = new System.Windows.Forms.Label();
+            this.txtNomeColunas = new System.Windows.Forms.TextBox();
+            this.lblNomeColunas = new System.Windows.Forms.Label();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.cbxFiltro = new System.Windows.Forms.ComboBox();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.txtNomeTabela = new System.Windows.Forms.TextBox();
+            this.lblNomeTabela = new System.Windows.Forms.Label();
+            this.btnAbrirDiretorio = new System.Windows.Forms.Button();
+            this.lblCopyRight = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnImportarPlanilha
+            // 
+            this.btnImportarPlanilha.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnImportarPlanilha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportarPlanilha.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnImportarPlanilha.Location = new System.Drawing.Point(174, 138);
+            this.btnImportarPlanilha.Name = "btnImportarPlanilha";
+            this.btnImportarPlanilha.Size = new System.Drawing.Size(166, 23);
+            this.btnImportarPlanilha.TabIndex = 6;
+            this.btnImportarPlanilha.Text = "Importar Planilha";
+            this.btnImportarPlanilha.UseVisualStyleBackColor = false;
+            this.btnImportarPlanilha.Click += new System.EventHandler(this.btnImportarPlanilha_Click);
+            // 
+            // btnAbrirPlanilha
+            // 
+            this.btnAbrirPlanilha.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAbrirPlanilha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirPlanilha.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAbrirPlanilha.Location = new System.Drawing.Point(174, 166);
+            this.btnAbrirPlanilha.Name = "btnAbrirPlanilha";
+            this.btnAbrirPlanilha.Size = new System.Drawing.Size(166, 23);
+            this.btnAbrirPlanilha.TabIndex = 7;
+            this.btnAbrirPlanilha.Text = "Abrir Planilha";
+            this.btnAbrirPlanilha.UseVisualStyleBackColor = false;
+            this.btnAbrirPlanilha.Click += new System.EventHandler(this.btnAbrirPlanilha_Click);
+            // 
+            // cbxTipoDeExcel
+            // 
+            this.cbxTipoDeExcel.FormattingEnabled = true;
+            this.cbxTipoDeExcel.Items.AddRange(new object[] {
+            ".xlsx",
+            ".xls"});
+            this.cbxTipoDeExcel.Location = new System.Drawing.Point(276, 48);
+            this.cbxTipoDeExcel.Name = "cbxTipoDeExcel";
+            this.cbxTipoDeExcel.Size = new System.Drawing.Size(64, 21);
+            this.cbxTipoDeExcel.TabIndex = 5;
+            // 
+            // txtNomeAquivo
+            // 
+            this.txtNomeAquivo.Location = new System.Drawing.Point(47, 48);
+            this.txtNomeAquivo.Name = "txtNomeAquivo";
+            this.txtNomeAquivo.Size = new System.Drawing.Size(223, 20);
+            this.txtNomeAquivo.TabIndex = 1;
+            // 
+            // lblNomeArquivo
+            // 
+            this.lblNomeArquivo.AutoSize = true;
+            this.lblNomeArquivo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNomeArquivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeArquivo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNomeArquivo.Location = new System.Drawing.Point(12, 9);
+            this.lblNomeArquivo.Name = "lblNomeArquivo";
+            this.lblNomeArquivo.Size = new System.Drawing.Size(0, 13);
+            this.lblNomeArquivo.TabIndex = 5;
+            // 
+            // lblDescricao
+            // 
+            this.lblDescricao.AutoSize = true;
+            this.lblDescricao.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDescricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescricao.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblDescricao.Location = new System.Drawing.Point(48, 29);
+            this.lblDescricao.Name = "lblDescricao";
+            this.lblDescricao.Size = new System.Drawing.Size(104, 13);
+            this.lblDescricao.TabIndex = 6;
+            this.lblDescricao.Text = "Nome do Arquivo";
+            // 
+            // txtNomeColunas
+            // 
+            this.txtNomeColunas.Location = new System.Drawing.Point(47, 94);
+            this.txtNomeColunas.Name = "txtNomeColunas";
+            this.txtNomeColunas.Size = new System.Drawing.Size(293, 20);
+            this.txtNomeColunas.TabIndex = 2;
+            // 
+            // lblNomeColunas
+            // 
+            this.lblNomeColunas.AutoSize = true;
+            this.lblNomeColunas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeColunas.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNomeColunas.Location = new System.Drawing.Point(50, 75);
+            this.lblNomeColunas.Name = "lblNomeColunas";
+            this.lblNomeColunas.Size = new System.Drawing.Size(113, 13);
+            this.lblNomeColunas.TabIndex = 8;
+            this.lblNomeColunas.Text = "Colunas da Tabela";
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltro.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblFiltro.Location = new System.Drawing.Point(44, 171);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(84, 13);
+            this.lblFiltro.TabIndex = 9;
+            this.lblFiltro.Text = "Filtro INT BIT";
+            // 
+            // cbxFiltro
+            // 
+            this.cbxFiltro.FormattingEnabled = true;
+            this.cbxFiltro.Items.AddRange(new object[] {
+            "@",
+            "#"});
+            this.cbxFiltro.Location = new System.Drawing.Point(47, 188);
+            this.cbxFiltro.Name = "cbxFiltro";
+            this.cbxFiltro.Size = new System.Drawing.Size(81, 21);
+            this.cbxFiltro.TabIndex = 4;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHelp.Location = new System.Drawing.Point(134, 188);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(29, 23);
+            this.btnHelp.TabIndex = 9;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // txtNomeTabela
+            // 
+            this.txtNomeTabela.Location = new System.Drawing.Point(47, 138);
+            this.txtNomeTabela.Name = "txtNomeTabela";
+            this.txtNomeTabela.Size = new System.Drawing.Size(105, 20);
+            this.txtNomeTabela.TabIndex = 3;
+            // 
+            // lblNomeTabela
+            // 
+            this.lblNomeTabela.AutoSize = true;
+            this.lblNomeTabela.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNomeTabela.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNomeTabela.Location = new System.Drawing.Point(44, 122);
+            this.lblNomeTabela.Name = "lblNomeTabela";
+            this.lblNomeTabela.Size = new System.Drawing.Size(100, 13);
+            this.lblNomeTabela.TabIndex = 11;
+            this.lblNomeTabela.Text = "Nome da Tabela";
+            // 
+            // btnAbrirDiretorio
+            // 
+            this.btnAbrirDiretorio.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAbrirDiretorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirDiretorio.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAbrirDiretorio.Location = new System.Drawing.Point(174, 195);
+            this.btnAbrirDiretorio.Name = "btnAbrirDiretorio";
+            this.btnAbrirDiretorio.Size = new System.Drawing.Size(166, 23);
+            this.btnAbrirDiretorio.TabIndex = 8;
+            this.btnAbrirDiretorio.Text = "Diretório Padrão";
+            this.btnAbrirDiretorio.UseVisualStyleBackColor = false;
+            this.btnAbrirDiretorio.Click += new System.EventHandler(this.btnAbrirDiretorio_Click);
+            // 
+            // lblCopyRight
+            // 
+            this.lblCopyRight.AutoSize = true;
+            this.lblCopyRight.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCopyRight.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblCopyRight.Location = new System.Drawing.Point(105, 227);
+            this.lblCopyRight.Name = "lblCopyRight";
+            this.lblCopyRight.Size = new System.Drawing.Size(235, 13);
+            this.lblCopyRight.TabIndex = 15;
+            this.lblCopyRight.Text = "Copyright © YYYY Vasconcellos IT Solutions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(389, 248);
+            this.Controls.Add(this.lblCopyRight);
+            this.Controls.Add(this.btnAbrirDiretorio);
+            this.Controls.Add(this.lblNomeTabela);
+            this.Controls.Add(this.txtNomeTabela);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.cbxFiltro);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.lblNomeColunas);
+            this.Controls.Add(this.txtNomeColunas);
+            this.Controls.Add(this.lblDescricao);
+            this.Controls.Add(this.lblNomeArquivo);
+            this.Controls.Add(this.txtNomeAquivo);
+            this.Controls.Add(this.cbxTipoDeExcel);
+            this.Controls.Add(this.btnAbrirPlanilha);
+            this.Controls.Add(this.btnImportarPlanilha);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gerador de Script SQL INSERT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportarPlanilha;
+        private System.Windows.Forms.Button btnAbrirPlanilha;
+        private System.Windows.Forms.ComboBox cbxTipoDeExcel;
+        private System.Windows.Forms.TextBox txtNomeAquivo;
+        private System.Windows.Forms.Label lblNomeArquivo;
+        private System.Windows.Forms.Label lblDescricao;
+        private System.Windows.Forms.TextBox txtNomeColunas;
+        private System.Windows.Forms.Label lblNomeColunas;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.ComboBox cbxFiltro;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.TextBox txtNomeTabela;
+        private System.Windows.Forms.Label lblNomeTabela;
+        private System.Windows.Forms.Button btnAbrirDiretorio;
+        private System.Windows.Forms.Label lblCopyRight;
+    }
+}
+
---
+++ GeradorInsertSQL/GeradorInsertSQL/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices; // ativamento da leitura do excell MARSHAL
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Text;
+
+namespace GeradorInsertSQL
+{
+    public partial class Form1 : Form
+    {
+        string folder = @"C:\www.vasconcellos.site\ScriptSQL\";
+        public Form1()
+        {
+            InitializeComponent();
+            cbxTipoDeExcel.SelectedIndex = 0;
+            cbxFiltro.SelectedIndex = 0;
+     
+            if (!Directory.Exists(folder))
+            {
+
+                Directory.CreateDirectory(folder);
+                MessageBox.Show(String.Concat("O Diretório ", folder,
+                    " foi criado com sucesso! Por favor utilize este diretório para armazenar a planilha que será importada."));
+            }
+
+            DateTime thisDay = DateTime.Today;
+            lblCopyRight.Text = "Copyright © " + thisDay.Year + " Vasconcellos IT Solutions";
+        }
+
+        private void btnImportarPlanilha_Click(object sender, EventArgs e)
+        {
+            //TESTE ------ SE OS CAMPOS ESTIVEREM VAZIOS O SISTEMA REALIZA O TESTE ATRAVÉS DA PLANILHA DE TESTE
+            if ((txtNomeAquivo.Text == "") && (txtNomeTabela.Text == "" && txtNomeColunas.Text == ""))
+            {
+                //HttpContext.Current.Server.MapPath(@"~\Excel\")
+                txtNomeAquivo.Text = "InsertSQLTeste";
+                txtNomeColunas.Text = "NOME,@RA,@MEDIA,@ATIVO,SALA";
+                txtNomeTabela.Text = "ALUNOS";
+                cbxFiltro.Text = "@";
+                cbxTipoDeExcel.Text = ".xlsx";
+
+                //OBTÉM O DIRETÓRIO DO .exe DA APLICAÇÃO
+                //string caminhox = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+                //OBTÉM O DIRETÓRIO DO FOLDER \Excel DA APLICAÇÃO
+                Environment.CurrentDirectory = @"..\..\Excel";
+                string fullPath = Directory.GetCurrentDirectory();
+
+                string excelTeste = 
+                    Path.Combine(fullPath,String.Concat(txtNomeAquivo.Text , cbxTipoDeExcel.Text));
+
+                if(!File.Exists(String.Concat(folder, txtNomeAquivo.Text, cbxTipoDeExcel.Text)))
+                { 
+                    File.Copy(excelTeste, String.Concat(folder, txtNomeAquivo.Text, cbxTipoDeExcel.Text));
+                }
+
+            }
+            //TESTE
+
+            string caminhoimportacao = String.Concat(folder, txtNomeAquivo.Text, cbxTipoDeExcel.Text);
+            lblNomeArquivo.Text = caminhoimportacao;
+            
+            if ((txtNomeAquivo.Text != "" && File.Exists(caminhoimportacao)) && txtNomeTabela.Text != "")
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(String.Concat(folder, txtNomeAquivo.Text, cbxTipoDeExcel.Text));
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+                ///string ValorDaCelula = xlWorksheet.Cells[1, 1].Value.ToString(); Obtendo Valor da [Coluna,Linha]
+                StringBuilder texto  = new StringBuilder();
+                string espaco = " ";
+
+                string filtrostr = cbxFiltro.Text;
+                string i_colunasfixo = "";
+                string i_colunasmeio = (txtNomeColunas.Text == "")? i_colunasfixo : txtNomeColunas.Text;
+
+                string i_inicio = String.Concat("INSERT INTO", espaco);
+                string i_tabela = String.Concat(txtNomeTabela.Text, espaco);
+                string i_colunas = String.Concat("(", i_colunasmeio, ")", espaco);
+                string i_value = "VALUES (";
+                string i_final = String.Concat("); ", Environment.NewLine, Environment.NewLine);
+                int qtdcharcol = i_colunas.Length;
+
+                int countvirgula = 0;
+                for (int i = 0; i < qtdcharcol; i++)
+                {
+                    if (i_colunas[i] == ',')
+                    {
+                        countvirgula++;
+                    };
+                }
+
+                if (colCount != (countvirgula + 1))
+                {
+                    MessageBox.Show("O número de colunas digitadas no campo do sistema, difere do número de colunas do Excel importado.");
+                }
+                else
+                {
+                    //Definindo INT BIT para não colocar aspas simples no ScriptSQL
+                    int[] positionsvirgula = new int[countvirgula];
+                    countvirgula = 0;
+                    int count = 0;
+                    int countpv = 0;
+                    for (int i = 0; i < qtdcharcol; i++)
+                    {
+                        if (i_colunas[i] == ',')
+                        {
+                            countvirgula++;
+                            if (Convert.ToInt32(i_colunas[i + 1]) == Convert.ToInt32(filtrostr[0]))
+                            {
+                                positionsvirgula[count++] = countvirgula + 1;//{}
+                                countpv++;//qtd
+                            }
+                        };
+
+                    }
+
+                    string i_colunasfiltro = i_colunas.Replace(filtrostr, "");
+                    int countpv2 = 0;
+                    string i_dados = "";
+
+                    try
+                    {
+                        for (int i = 1; i <= rowCount; i++)
+                        {
+                            i_dados = "";
+                            countpv2 = 0;
+                            for (int j = 1; j <= colCount; j++)
+                            {
+                                if (String.Equals(xlWorksheet.Cells[i, j].Value, "NULL"))
+                                {
+                                    i_dados += xlWorksheet.Cells[i, j].Value;
+                                }
+                                else
+                                {
+                                    if (j == positionsvirgula[countpv2] && countpv2 <= countpv)
+                                    {
+                                        i_dados += (xlWorksheet.Cells[i, j].Value != null) ? xlWorksheet.Cells[i, j].Value : "''";
+                                        countpv2++;
+                                    }
+                                    else
+                                    {
+                                        i_dados += (xlWorksheet.Cells[i, j].Value != null) ?
+                                            String.Concat("'", xlWorksheet.Cells[i, j].Value, "'") : "''";
+                                    }
+                                }
+
+                                if (j != colCount)
+                                {
+                                    i_dados += ",";
+                                }
+                            }
+
+                            texto.
+                                Append(i_inicio).
+                                Append(i_tabela).
+                                Append(i_colunasfiltro).
+                                Append(i_value).
+                                Append(i_dados).
+                                Append(i_final);
+
+                        }
+                        GerarArquivo(texto.ToString(), folder, i_tabela);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Erro, por favor verifique se as informações fornecidas ao sistema, estão corretas");
+                    }
+                }
+
+                //FECHANDO OS PROCESSOS
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                //OBS:nunca use dois pontos, todos os objetos COM devem ser referenciados e liberados individualmente
+                //Liberar objetos COM para matar completamente processo de execução do excel em segundo plano
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+                //FECHANDO OS PROCESSOS
+            }
+            else 
+            {
+                MessageBox.Show(String.Concat("1 - Verifique se o arquivo que você deseja, existe dentro do diretório ",
+                    folder, Environment.NewLine, Environment.NewLine,
+                "2 - Escreva o nome da Tabela que você deseja criar o Insert no campo Nome da Tabela"));
+            }
+
+        }
+
+        public void GerarArquivo(string texto, string folder, string i_tabela)
+        {
+            //---Gerando INSERT-SQL.TXT
+            string tipoarquivo = ".txt";
+            string nomedoarquivo = String.Concat("INSERT-SQL - TABELA ", i_tabela);
+            string caminhodoarquivo = String.Concat(folder, nomedoarquivo, tipoarquivo);
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(caminhodoarquivo))
+            {
+                file.Write(texto);
+                MessageBox.Show(String.Concat(" O ", tipoarquivo, 
+                    " do INSERT-SQL foi gerado com sucesso! O seu arquivo foi salvo no diretório ", folder));
+                System.Diagnostics.Process.Start(caminhodoarquivo);
+            }
+            //---Gerando INSERT-SQL.TXT
+        }
+
+        private void btnAbrirPlanilha_Click(object sender, EventArgs e)
+        {
+            if (txtNomeAquivo.Text != "")
+            {
+                try
+                {
+                    System.Diagnostics.Process.Start(String.Concat(folder, txtNomeAquivo.Text, cbxTipoDeExcel.Text));
+                }
+                catch
+                {
+                    MessageBox.Show("Planilha não encontrada");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Escreva o nome da planilha que você deseja abrir");
+            }
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                String.Concat("Help ?", Environment.NewLine, Environment.NewLine, Environment.NewLine,
+                "0 - Clique em Importar Planilha sem preencher os dados para realizar um TESTE ;", Environment.NewLine, Environment.NewLine,
+                "1 - Escreva o nome da planilha no campo Nome do Arquivo ;", Environment.NewLine, Environment.NewLine,
+                "2 - Selecione o tipo de arquivo na caixa de seleção ;", Environment.NewLine, Environment.NewLine,
+                "3 - Escreva os nomes das colunas no campo Colunas da Tabela ;", Environment.NewLine, Environment.NewLine,
+                "4 - Escreva o nome da Tabela que você deseja criar o INSERT no campo Nome da Tabela ;", Environment.NewLine, Environment.NewLine,
+                "5 - Selecione o tipo de filtro usado para Colunas do type INT,BIT,DECIMAL... ;", Environment.NewLine, Environment.NewLine,
+                "6 - Após selecionar o filtro, escreva o mesmo caractere contido no campo de seleção,",
+                    " no campo Coluna da Tabela para cada coluna dos tipos acima ;", Environment.NewLine, Environment.NewLine,
+                "7 - Clique no botão Importar Planilha.", Environment.NewLine, Environment.NewLine,
+                "8 - Todos os arquivos de importação e geração do sistema, deverão ser armazenados no diretório ", folder));
+        }
+
+        private void btnAbrirDiretorio_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(folder);
+            }
+            catch
+            {
+                MessageBox.Show("Diretório não encontrador");
+            }
+
+        }
+
+    }
+}
